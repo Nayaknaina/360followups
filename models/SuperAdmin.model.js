a1e7b1eb8c65db7a5ff7304958819175866699c1
Nayaknaina <nainanayak288@.com>
2024-11-15 13:15:39 +0530

    superadmin started

---
+++ models/SuperAdmin.model.js
@@ -0,0 +1,37 @@
+// models/SuperAdmin
+
+const mongoose = require('mongoose');
+
+const superAdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    max:25,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+  },
+  mobileNumber: {
+    type: String,
+    required: [true, 'Mobile number is required'],
+    unique: true,
+    match: [/^\d{10}$/, 'Mobile number must be 10 digits long'],
+  },
+ 
+  signUpDate: {
+    type: Date,
+    default: Date.now,
+  },
+}, {
+  timestamps: true, 
+});
+
+const SuperAdmin = mongoose.model('SuperAdmin', superAdminSchema);
+
+module.exports = SuperAdmin;
